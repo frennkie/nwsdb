edd5fc59f2d9d7829627084b6a92c91df04611ff
Robert Habermann <mail@rhab.de>
2015-10-07 17:36:27 +0200

    remove clean up task as this is done anyway by backend

--- nwscandb/tasks.py
+++ nwscandb/tasks.py
@@ -1,4 +1,4 @@
-from celery import Task, task, current_task
+from celery import task, current_task
 from nwscandb.celeryapp import celery_pipe as celery
 from libnmap.process import NmapProcess
 from nwscandb.models import NmapReportMeta
@@ -49,20 +49,3 @@ def celery_nmap_store_report(task_id):
     else:
         return {"rc": 1}
 
-
-# Class for a task
-class CleanupTask(Task):
-    """
-    Examples:
-
-    >>> from nwscandb.tasks import CleanupTask
-    >>> result = CleanupTask.delay()
-
-    """
-    # do not store result in backend
-    ignore_result = True
-
-    def run(self, **kwargs):
-        """ Run method of the class """
-        # Cleanup here
-        self.backend.cleanup()
