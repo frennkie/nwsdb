40236b63b670f36a3b504dbedaeaf35f638141b1
Robert Habermann <mail@rhab.de>
2016-03-28 12:19:54 +0200

    misc

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,6 @@
 from fabric.api import *
+from fabric.utils import puts
+from fabric.utils import fastprint
 from fabric.contrib.files import *
 from fabric.contrib.project import rsync_project
 from fabric.contrib.project import upload_project
@@ -13,6 +15,7 @@ env.hosts = ["localhost"]
 
 # Where your project code lives on the local machine
 env.project_root = "/home/robbie/work/nmap_tool/nwsdb"
+env.project_name = "nwsdb"
 
 # Where the static files get collected locally. Your STATIC_ROOT setting.
 ### pretty, pretty please.. call your STATIC_ROOT "static"
@@ -27,13 +30,21 @@ LOCAL_ROOT = os.path.dirname(os.path.join(env.project_root, ''))
 LOCAL_STATIC_DIR_NAME = "static"
 
 REMOTE_ROOT = os.path.dirname(os.path.join(env.remote_root, ''))
-#REMOTE_DIR_APP = os.path.join(REMOTE_ROOT, "nwscandb/")
+#REMOTE_DIR_PROJECT = os.path.join(REMOTE_ROOT, env.project_name)
 #REMOTE_DIR_VENV = os.path.join(REMOTE_ROOT, "venv/")
 
 
 def remove_dest_dir(dest_dir):
     if exists(dest_dir):
         sudo("rm -rf {0}".format(dest_dir))
+
+def clean_up_remote(dest_dir):
+    remove_dest_dir("{0}/nwsdb/.git".format(dest_dir))
+    remove_dest_dir("{0}/nwsdb/common-static".format(dest_dir))
+    remove_dest_dir("{0}/nwsdb/fabfile.py".format(dest_dir))
+    remove_dest_dir("{0}/nwsdb/fabfile.pyc".format(dest_dir))
+    remove_dest_dir("{0}/nwsdb/nwscandb/settings_dev.py".format(dest_dir))
+    remove_dest_dir("{0}/nwsdb/nwscandb/settings_dev_secret.py".format(dest_dir))
 
 def deploy_static(local_dir, dest_dir):
     with cd(env.project_root):
@@ -46,27 +57,46 @@ def deploy_static(local_dir, dest_dir):
 def deploy_app(local_dir, dest_dir):
     upload_project(local_dir, dest_dir, use_sudo=True)
 
-def chown_dir(dest_dir, user="www-data"):
+def chown_dir(dest_dir, user):
     sudo("chown -R {0}:{0} {1}".format(user, dest_dir))
 
 def venv_deploy(dest_dir):
     sudo("virtualenv -p /usr/bin/python3.4 {0}/venv".format(dest_dir))
 
 def venv_install_requirements(dest_dir):
-    with settings(sudo_user="robbie"):
+    with settings(sudo_user=env.user):
         run("{0}/venv/bin/pip install -U pip".format(dest_dir))
         run("{0}/venv/bin/pip install -U -r {0}/nwsdb/requirements.txt".format(dest_dir))
 
 def deploy_all(local_root=LOCAL_ROOT, remote_root=REMOTE_ROOT):
     #
+    puts("--- Deploying from this host to \"{0}\" as {1}  ---".format(env.host, env.user))
+
     remove_dest_dir(remote_root)
     sudo("mkdir -p {0}".format(remote_root))
     deploy_static(local_root, remote_root)
     deploy_app(local_root, remote_root)
 
     venv_deploy(remote_root)
-    chown_dir(remote_root, user="robbie")
+    chown_dir(remote_root, user=env.user)
     venv_install_requirements(remote_root)
-    chown_dir(remote_root)
+    chown_dir(remote_root, user="www-data")
 
+    sudo("cp {0}/nwsdb/nwscandb_prod.conf /etc/apache2/sites-available/".format(remote_root))
+    sudo("a2ensite nwscandb_prod")
+    sudo("service apache2 reload")
+
+
+def sync_project(local_root=LOCAL_ROOT, remote_root=REMOTE_ROOT):
+    remote_dir_project = os.path.join(remote_root, env.project_name)
+    puts("--- Sync from \"{0}\" to {1}  ---".format(local_root, remote_dir_project))
+    chown_dir(remote_dir_project, user=env.user)
+    rsync_project(remote_dir_project, local_root, exclude="nwsdb/.git")
+    chown_dir(remote_dir_project, user="www-data")
+    clean_up_remote(remote_root)
+    sudo("touch {0}/nwscandb/wsgi_prod.py".format(remote_dir_project))
+
+
+def testing():
+    pass
 
